99b5515d898257a9771d1eaab872f1f857eb3c06
Nagalokesh <lokesh.kanumoori@gmail.com>
2017-09-16 16:03:39 +0530

    Menu and Fragments

---
+++ app/src/main/java/com/lokeshlabs/location1/Location1.java
@@ -0,0 +1,29 @@
+package com.lokeshlabs.location1;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Location1 extends Fragment {
+
+
+    public Location1() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_location1, container, false);
+    }
+
+}
--- app/src/main/java/com/lokeshlabs/location1/MainActivity.java
+++ app/src/main/java/com/lokeshlabs/location1/MainActivity.java
@@ -1,7 +1,10 @@
 package com.lokeshlabs.location1;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getCurrentLocationUpdates();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.select_fragment, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_location1:
+                getCurrentLocationUpdates();
+                break;
+            case R.id.menu_location2:
+                getLastKnownLocation();
+                break;
+            case R.id.menu_activity_recognition:
+                getActivityRecognition();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void getCurrentLocationUpdates() {
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, new Location1())
+                .commit();
+    }
+
+    private void getLastKnownLocation() {
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, new Location2())
+                .commit();
+    }
+
+    private void getActivityRecognition() {
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, new ActivityRecognition())
+                .commit();
     }
 }
